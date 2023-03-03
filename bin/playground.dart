1618b7bcdc0971f17eb6094cfb5c6aac3fd19ce9
Junior Maia <maiajr2394@gmail.com>
2023-03-03 15:22:42 -0300

    correction of rule of three and refactories

--- bin/playground.dart
+++ bin/playground.dart
@@ -1,5 +1,7 @@
-import 'desafios.dart' as playground;
 import 'dart:io';
+import '../challenges/imc.dart';
+import '../challenges/fibonacci.dart';
+import '../challenges/rule_of_three.dart';
 
 void main(List<String> arguments) {
   print('SELECIONE UMA DAS OPÇÕES ABAIXO:');
@@ -13,7 +15,7 @@ void main(List<String> arguments) {
 
   while (continuar) {
     if (input == 1) {
-      print({playground.imc()});
+      print({imc()});
       print('Deseja continuar?(s/n)');
       var inputContinuar = stdin.readLineSync()!;
       inputContinuar == 's' ? continuar : continuar = false;
@@ -21,12 +23,12 @@ void main(List<String> arguments) {
       print('------- FIBONACCI -------');
       print('Digite o valor de n:');
       var n = int.parse(stdin.readLineSync()!);
-      print({playground.fibonacci(n)});
+      print({fibonacci(n)});
       print('Deseja continuar?(s/n)');
       var inputContinuar = stdin.readLineSync()!;
       inputContinuar == 's' ? continuar : continuar = false;
     } else if (input == 3) {
-      print({playground.regraDe3()});
+      print({ruleOfThree()});
       print('Deseja continuar?(s/n)');
       var inputContinuar = stdin.readLineSync()!;
       inputContinuar == 's' ? continuar : continuar = false;
