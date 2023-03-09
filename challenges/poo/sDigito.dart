7564f515ab238136af9b48f57c68439610052a63
Junior Maia <maiajr2394@gmail.com>
2023-03-08 21:12:06 -0300

    finished

--- challenges/poo/sDigito.dart
+++
@@ -1,8 +0,0 @@
-import 'pDigito.dart';
-
-class sDigito extends Digito {
-  sDigito({
-    required String digitos,
-  }) : super(digitos: digitos);
-}
-//concluir o restante da classe tentando fazer extends
